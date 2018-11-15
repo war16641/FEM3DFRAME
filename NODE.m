54dd8228a701140a4ad1724b70870054a99ca8b2
war16641 <1627380717@qq.com>
2018-11-15 11:17:25 +0800

    映射矩阵 引入VCM

--- NODE.m
+++ NODE.m
@@ -4,9 +4,9 @@ classdef NODE<handle
     properties
         f FEM3DFRAME%隶属于哪一个有限元
         nds double%4列 第一列为id
-        nds_force double%节点力(外界对节点的力) FEM.solve操作 第一列是节点编号
-        nds_displ double%节点位移 FEM.solve操作 第一列是节点编号 2~7是位移
-        nds_mapping double%节点编号与刚度矩阵的映射 第一列是节点编号 第二列是节点x自由度对应的序号 FEM2D.solve操作
+
+        nds_mapping %节点编号与刚度矩阵的映射 第一列是节点编号 第二列是节点x自由度对应的序号 FEM2D.solve操作
+        nds_mapping_r %nds_mapping的反矩阵 第一列是序号 第二列是节点
         maxnum%使用的最大编号
         ndnum%节点个数
     end
@@ -16,7 +16,8 @@ classdef NODE<handle
             obj.f=f;
             obj.maxnum=0;
             obj.ndnum=0;
-
+            obj.nds_mapping=VCM.VALUE_CLASS_MANAGER_UNIQUE_SORTED();
+            obj.nds_mapping_r=VCM.VALUE_CLASS_MANAGER_UNIQUE_SORTED();
             
         end
         function AddByCartesian(obj,id,x,y,z)
@@ -133,43 +134,10 @@ classdef NODE<handle
             error('未找到节点');
         end
         function xuhao=GetXuhaoByID(obj,id)%通过id获得刚度矩阵中的序号 该节点ux对于的序号
-            %% 如果id直接大于了节点数量倒着搜索
-            if id>obj.ndnum
-                for it=obj.ndnum:-1:1
-                    if obj.nds_mapping(it,1)==id
-                        xuhao=obj.nds_mapping(it,2);
-                        return;
-                    end
-                end
-                error('未找到节点');
-            end
-            
-            
-            %% 首先返回id处的坐标 如果符合的话
-            if obj.nds_mapping(id,1)==id
-                xuhao=obj.nds_mapping(id,2);
-                return;
-            end
-            %% 不符合
-            if obj.nds_mapping(id,1)>id%如果这里比目标大 向前搜索
-                for it=id-1:-1:1
-                    if obj.nds_mapping(it,1)==id
-                        xuhao=obj.nds_mapping(it,2);
-                        return;
-                    end
-                end
-                error('未找到节点');
-            elseif  obj.nds_mapping(id,1)>id%如果这里比目标小 向后搜索
-                for it=id+1:obj.ndnum
-                    if obj.nds_mapping(it,1)==id
-                        xuhao=obj.nds(it,2);
-                        return;
-                    end
-                end
-                error('未找到节点');
-            end
-            %% 1
-            error('未找到节点');
+          xuhao=obj.nds_mapping.Get('id',id);
+          if isempty(xuhao)
+              error('无此节点');
+          end
         end
         function [id,index,label]=GetIdByXuhao(obj,xh)%通过刚度矩阵中的序号获得节点id 
             %先将xh放到ux上 并计算index和label
@@ -192,24 +160,22 @@ classdef NODE<handle
             
             %计算id
             xh=xh-yushu+1;%将序号移动到本节点的ux自由度上
-            for it=1:size(obj.nds_mapping,1)%算法可能有待改进
-                if xh==obj.nds_mapping(it,2)
-                    id=obj.nds_mapping(it,1);
-                    return;
-                end
+            id=obj.nds_mapping_r.Get('id',xh);
+            if isempty(id)
+                error('未找到')
             end
-            error('matlab:error','未找到')
         end
         function SetupMapping(obj)%建立节点自由度对刚度矩阵(K)的映射 
             %这个函数执行应在solve中调用
             %此函数执行后 不应再对节点进行操作了
-            obj.nds_mapping=zeros(obj.ndnum,2);
             lastx=-5;
             for it=1:obj.ndnum
-                obj.nds_mapping(it,1)=obj.nds(it,1);
                 lastx=lastx+6;
-                obj.nds_mapping(it,2)=lastx;
+                obj.nds_mapping.Append(obj.nds(it,1),lastx);
+                obj.nds_mapping_r.Append(lastx,obj.nds(it,1));
             end
+            obj.nds_mapping.Check();
+            obj.nds_mapping_r.Check();
         end
         function LoadFromMatrix(obj,mt)%从矩阵中载入 第一列是id 二三是xy %需要更改
             for it=1:size(mt,1)
