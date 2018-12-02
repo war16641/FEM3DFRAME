28a909d8bcb3732d684e023e20a26a3e30d4fca4
war16641 <1627380717@qq.com>
2018-12-02 14:01:49 +0800

    未命名

--- LoadCase_Modal.m
+++ LoadCase_Modal.m
@@ -17,101 +17,18 @@ classdef LoadCase_Modal<LoadCase
             obj.arg={[],'k'};
         end
         function Solve(obj)
-            %形成节点与刚度矩阵的映射
-            obj.f.node.SetupMapping();
-            
-            obj.GetK();
-            obj.GetM();
-            
-            %检查边界条件是否重复
-            obj.bc.Check();
-            
-            %K1为引入边界条件的总刚度矩阵
-            obj.dof=size(obj.K,1);
-            u=zeros(obj.dof,1);
-            
-            %先处理位移边界
-            df=zeros(obj.dof,1);
-            
-            in=[];%存储不激活的自由度 位移限制的自由度
-            for it=1:obj.bc.displ.num
-                ln=obj.bc.displ.Get('index',it);
-                index=obj.f.node.GetXuhaoByID(ln(1))+ln(2)-1;%得到序号
-                df=df-obj.K(:,index)*ln(3);
-                u(index)=ln(3);%保存位移
-                if ln(3)~=0
-                    error('matlab:myerror','自振工况不能出现位移不为0的边界条件')
-                end
-                in=[in index];
-            end
-            obj.activeindex=1:obj.dof;
-            
-            %处理未被单元激活自由度
-            hit=zeros(obj.dof,1);%自由度被击中次数
-            
-            for it=1:obj.f.manager_ele.num
-                e=obj.f.manager_ele.Get('index',it);
-                e.CalcHitbyele();
-                for it1=1:length(e.nds)
-                    xh=obj.f.node.GetXuhaoByID(e.nds(it1));
-                    hit(xh:xh+5)=hit(xh:xh+5)+e.hitbyele(it1,:)';%hit加1
-                end
-            end
-            %收集未被单元激活的自由度
-            tmp=1:obj.dof;
-            deadindex=tmp(hit==0);
-            %输出未被单元激活的自由度信息
-            if ~isempty(deadindex)
-                disp('存在未被单元激活的自由度')
-            end
-            for it=1:length(deadindex)
-                [id,~,label]=obj.f.node.GetIdByXuhao(deadindex(it));
-                disp(['节点' num2str(id) ' ' label]);
-            end
-            
-            %位移荷载对应的自由度与未被单元激活的自由度是否重叠 当自由度缺少的单元在边界处时 会出现这种情况
-            [~,ia,~]=unique([in deadindex]);
-            if ia<length(in)+length(deadindex)
-                warning('位移荷载对应的自由度与未被单元激活的自由度重叠。（当自由度缺少的单元在边界处时会出现这种情况，这是正常的，其他是异常的。')
-            end
-            %删除两种类型未激活的自由度
-            obj.activeindex([in deadindex])=[];
-            
-            %处理力边界条件
-            index_force=[];%力荷载 击中的自由度序号
-            ft=zeros(obj.dof,1);
-            for it=1:obj.bc.force.num
-                ln=obj.bc.force.Get('index',it);
-                index=obj.f.node.GetXuhaoByID(ln(1))+ln(2)-1;
-                if ln(3)~=0
-                    error('matlab:myerror','自振工况不能出现力不为0的边界条件')
-                end
-                ft(index)=ft(index)+ln(3);
-                index_force=[index_force index];
-            end
-            f1=ft+df;
-            
-            %检查力是否加载在未被单元激活的自由度上
-            [~,ia,~]=unique([index_force deadindex]);
-            if length(index_force)+length(deadindex)>length(ia)
-                error('matlab:myerror','力加载在未被单元激活的自由度上')
-            end
-            
-            %形成刚度 质量 矩阵 引入边界条件后
-            K1=obj.K(obj.activeindex,obj.activeindex);%去除一部分自由度
-            M1=obj.M(obj.activeindex,obj.activeindex);
-            
+            obj.PreSolve();
             %调用算法求解特征值
-            [obj.w,obj.mode]=LoadCase_Modal.GetInfoForFreeVibration_eig(K1,M1,obj.arg{1},obj.arg{2});
+            [obj.w,obj.mode]=LoadCase_Modal.GetInfoForFreeVibration_eig(obj.K1,obj.M1,obj.arg{1},obj.arg{2});
             if isempty(obj.arg{1})
-                obj.arg{1}=size(K1,1);
+                obj.arg{1}=size(obj.K1,1);
             end
             %处理求解后所有自由度上的力和位移 保存结果
             tic
+            u1=obj.u_beforesolve;
             for it=1:length(obj.w)%这一块循环有点花时间
                 w1=obj.w(it);
                 mode1=obj.mode(:,it);
-                u1=u;
                 u1(obj.activeindex)=mode1;
                 f1=obj.K*u1;
                 obj.rst.Add(it,w1,f1,u1);
@@ -124,15 +41,16 @@ classdef LoadCase_Modal<LoadCase
             
             %初始化结果指针
             obj.rst.SetPointer();
+            
+            
+            
+            
+            
+
 
         end
         function GetK(obj)
             %K是总刚度矩阵(边界条件处理前) 阶数为6*节点个数
-            
-            
-
-            
-            
             obj.K=zeros(6*obj.f.node.ndnum,6*obj.f.node.ndnum);
             f=waitbar(0,'组装刚度矩阵','Name','FEM3DFRAME');
             for it=1:obj.f.manager_ele.num
@@ -145,10 +63,6 @@ classdef LoadCase_Modal<LoadCase
         end
         function GetM(obj)%组装质量矩阵（边界条件引入之前) 务必先调用GetM形成映射
             %M是总质量矩阵(边界条件处理前) 阶数为6*节点个数
-            
-            
-            
-            
             obj.M=zeros(6*obj.f.node.ndnum,6*obj.f.node.ndnum);
             f=waitbar(0,'组装质量矩阵','Name','FEM3DFRAME');
             for it=1:obj.f.manager_ele.num
@@ -158,6 +72,15 @@ classdef LoadCase_Modal<LoadCase
                 
             end
             close(f);
+        end
+        function CheckBC(obj)
+            %要求位移边界条件全是0
+            for it=1:obj.bc.displ.num
+                ln=obj.bc.displ.Get('index',it);
+                if ln(3)~=0
+                    error('matlab:myerror','自振工况不能出现位移不为0的边界条件')
+                end
+            end
         end
     end
     methods(Static)
--- Untitled5.m
+++ Untitled5.m
@@ -3,18 +3,61 @@ close all
 
 f=FEM3DFRAME();
 f.node.AddByCartesian(1,0,0,0);
-f.node.AddByCartesian(2,0,0,1.2);
-tmp=ELEMENT_SPRING(f,0,[1 2],[1.15 0.8 0 0 0 0]);
-tmp.SetNLProperty(1,[1 1 0.1]);
+f.node.AddByCartesian(2,1,0,0);
+
+m=1;
+
+tmp=ELEMENT_MASS(f,0,2,[m m m 0 0 0]);
 f.manager_ele.Add(tmp);
 
-lc=LoadCase_Static(f,'dead');
+
+k=1;
+tmp=ELEMENT_SPRING(f,0,[1 2],[k 0 0 0 0 0]);
+f.manager_ele.Add(tmp);
+
+
+lc1=LoadCase_Modal(f,'modal');
+f.manager_lc.Add(lc1);
+lc1.AddBC('displ',[1 1 0;1 2 0;1 3 0;1 4 0;1 5 0;1 6 0]);
+lc1.Solve();
+[~,pri]=lc1.rst.GetPeriodInfo();
+w1=pri(3);
+
+lcd=LoadCase_Static(f,'eq');
+f.manager_lc.Add(lcd);
+lcd.CloneBC(lc1);
+lcd.AddBC('force',[2 1 1]);
+lcd.Solve();
+
+
+lc=LoadCase_Earthquake(f,'eq');
 f.manager_lc.Add(lc);
+lc.CloneBC(lcd);
+
+ lc.intd.Add([2 1 1])
 
 
-lc.AddBC('displ',[1 1 0;1 2 0;1 3 0;1 4 0;1 5 0;1 6 0;]);
-lc.AddBC('force',[2 3 1.5]);
 
+ew=EarthquakWave.MakeConstant(0,20,0.01);
+ei=EarthquakeInput(lc,'const',ew,1,0);
+lc.AddEarthquakeInput(ei);
+lc.SetAlgorithm('newmark',0.5,0.25);
+[a, b]=DAMPING.RayleighDamping(w1,5,0.005,0.005);
+
+lc.damp.Set('rayleigh',a,b);
 lc.Solve();
-lc.rst.Get('node','displ',2,3)
-lc.rst.Get('ele','force',1,'i','all')
+[vn,tn]=lc.rst.GetTimeHistory(0,40,'node','displ',2,1);
+figure
+plot(tn,vn)
+
+%解析解
+[v]=dsolve('D2y+0.01*Dy+y=1','Dy(0)=0','y(0)=1');
+syms t
+v_v=subs(v,t,[0:0.01:20]);
+v_v=double(v_v);
+hold on
+plot(0:0.01:20,v_v,'o','markersize',2);
+legend('fem','解析解')
+er=norm(vn-v_v');
+
+testcase.verifyTrue(er/2001<0.002,'验证错误');
